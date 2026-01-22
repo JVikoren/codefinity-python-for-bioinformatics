b485aca329b031bed52a8f146691d72ae8d3f73e
JVikoren <>
2026-01-22 00:14:06 +0000

    Challenge: Differential Expression (Simplified)

--- gene_expression_analysis_and_reproducible_pipelines/challenge_differential_expression_simplified/main.py
+++ gene_expression_analysis_and_reproducible_pipelines/challenge_differential_expression_simplified/main.py
@@ -1,11 +1,9 @@
 def find_differentially_expressed_genes(control_counts, treatment_counts, gene_names, fold_change_threshold):
-    control_counts = [c if c != 0 else fold_change_threshold for c in control_counts]
-    fold_change = [t / c for t, c in zip(treatment_counts, control_counts)]
-    result = []
-    for i in range(len(gene_names)):
-        if fold_change[i] > fold_change_threshold:
-            result.append(gene_names[i])
-    return result
+    return [
+        gene
+        for c, t, gene in zip(control_counts, treatment_counts, gene_names)
+        if (t / c if c else t) > fold_change_threshold
+    ]
     
 
 control_counts = [100, 50, 200, 0]
